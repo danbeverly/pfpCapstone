fd7f1288b69b0293605ff2b7f0cf778ab4f183fd
Dan Beverly <60118678+danbeverly@users.noreply.github.com>
2022-03-30 21:37:20 -0400

    Update receive.py

--- receive.py
+++ receive.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+# Listening program waits to receive signal from the defensive laptop to trigger reboot 'remediation'
+
+# import necessary libraries
 import serial
 import os
 
+# set up serial communications
 ser = serial.Serial(
 	port = '/dev/ttyS0',
 	baudrate = 115200,
@@ -12,8 +16,10 @@ ser = serial.Serial(
 	timeout = 1
 )
 
+# read from serial and wait to receive signal to rebeoot
 while 1:
 	x = ser.readline()
 	x = x.decode('utf-8')
 	if x != '':
+		# execute reboot
 		os.system('reboot')
