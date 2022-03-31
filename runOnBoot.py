64b4471b49a4d62264fbb6df43198d6767cd3c0c
Dan Beverly <60118678+danbeverly@users.noreply.github.com>
2022-03-30 21:38:41 -0400

    Update runOnBoot.py

--- runOnBoot.py
+++ runOnBoot.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
-
-
+# This file runs when the pi reboots, sending a signal to the defensive computer that the system is back up and running
 
 
 while True:
+    # import needed libraries
     import serial
     import os
+    # set up serial communications
     ser = serial.Serial(
     port = '/dev/ttyS0',
     baudrate = 115200,
@@ -15,6 +16,7 @@ while True:
     bytesize = serial.EIGHTBITS,
     timeout = 1
 )
+    # send 'up' to the devensive computer to signal that the pi has rebooted
     ser.write('up'.encode())
 
 
